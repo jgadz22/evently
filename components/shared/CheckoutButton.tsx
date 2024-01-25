23b74cab804e48221e455c39bbdf4d91ad12721b
Joseph <gadiazajoseph18@gmail.com>
2024-01-25 18:13:28 +0800

    added some functionality and also some components

---
+++ components/shared/CheckoutButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { IEvent } from "@/lib/database/models/event.model";
+import { useUser } from "@clerk/nextjs";
+import React from "react";
+
+const CheckoutButton = ({ event }: { event: IEvent }) => {
+  const { user } = useUser();
+  const userId = user?.publicMetadata.userId as string;
+  const hasEventFinished = new Date(event.endDateTime) < new Date();
+
+  return (
+    <div className="flex items-center gap-3">
+      {hasEventFinished ? (
+        <p>Sorry, tickets are no longer available.</p>
+      ) : (
+        <>Button</>
+      )}
+    </div>
+  );
+};
+
+export default CheckoutButton;
